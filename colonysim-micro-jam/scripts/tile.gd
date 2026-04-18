2165d00bf5d2bf9fb4dd4eee4928973c45b41568
arrowira <41136800+arrowira@users.noreply.github.com>
2026-04-17 23:05:53 -0700

    random tile colors

--- colonysim-micro-jam/scripts/tile.gd
+++ colonysim-micro-jam/scripts/tile.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 var inMouse = false
-
+var special = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	special = randi_range(0,2)
+	$EquilateralTriangle.modulate = Color.DARK_BLUE
+	if special == 1:
+		$EquilateralTriangle.modulate.r = 0.2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
